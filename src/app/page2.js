e632d5fe3aab1134688ab503bd308164cb8d7173
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-02-03 02:50:05 +0500

    wip/added a file

---
+++ src/app/page2.js
@@ -0,0 +1,59 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import styles from "./page.module.css";
+import { getUniversitiesByCountry } from "@/apiActions/university/action.universities";
+import { getCountries } from "@/apiActions/country/action.countries";
+import DataTable from "@/components/Table/Table";
+import { Alert } from "antd";
+
+const Home = () => {
+  const [univeristiesData, setUniversitiesData] = useState([]);
+  const [countriesData, setCountriesData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    Promise.all([getUniversitiesByCountry(), getCountries()])
+      .then(([univerisities, countries]) => {
+        setUniversitiesData(univerisities);
+        setCountriesData(countries);
+      })
+      .catch((e) => {
+        setError(e);
+      });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {error ? (
+        <Alert message="Error" description={error} type="error" />
+      ) : (
+        <>
+          <h1>Data from University Api</h1>
+          {univeristiesData ? (
+            <DataTable
+              data={univeristiesData}
+              type="university"
+              setData={setUniversitiesData}
+            />
+          ) : (
+            <h1>loading...</h1>
+          )}
+
+          <h1>Data from Country Api</h1>
+          {countriesData ? (
+            <DataTable
+              data={countriesData}
+              type="country"
+              setData={setCountriesData}
+            />
+          ) : (
+            <h1>loading...</h1>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
